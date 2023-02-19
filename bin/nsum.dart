a97229b41d5543ce5c63714106d73546098b3682
Luke Pighetti <lukepighetti@gmail.com>
2023-02-19 16:36:28 -0500

    solve the height problem in millimeters

--- bin/nsum.dart
+++ bin/nsum.dart
@@ -1,7 +1,20 @@
 void main(List<String> arguments) {
-  final target = 28;
-  final choices = [3, 4, 5, 6, 7, 8, 11, 14].ascending;
-  final tolerance = 0;
+  final offset = 7;
+  final target = 898 - offset;
+
+  final inventory = {
+    535: 1,
+    329: 1,
+    315: 1,
+    230: 2,
+    180: 1,
+    158: 2,
+    130: 1,
+    105: 3,
+  };
+
+  final choices = inventory.keys.ascending;
+  final tolerance = 10;
 
   final results = <String>{};
 
@@ -12,7 +25,15 @@ void main(List<String> arguments) {
 
       if (nextSum.withinToleranceOf(tolerance, target)) {
         final result = [...curr, choice].ascending;
-        results.add('${result.sum}: ${result.join(',')}');
+        final tmpInventory = Map<int, int>.from(inventory);
+        for (final result in result) {
+          tmpInventory[result] = tmpInventory[result]! - 1;
+        }
+        final isValid = tmpInventory.values.every((e) => !e.isNegative);
+        if (isValid) {
+          results.add(
+              '${result.sum}(${result.sum - target}): ${result.join(',')}');
+        }
       }
 
       if (nextSum < target) {
@@ -25,7 +46,7 @@ void main(List<String> arguments) {
   visit();
   stopwatch.stop();
 
-  results.forEach(print);
+  results.ascending.forEach(print);
 
   print(
     'Found ${results.length} results '
@@ -33,7 +54,7 @@ void main(List<String> arguments) {
   );
 }
 
-extension ComparableList<T extends Comparable> on List<T> {
+extension ComparableIterable<T extends Comparable> on Iterable<T> {
   List<T> get ascending => List.from(descending.reversed);
   List<T> get descending => List.from(this)..sort((a, b) => b.compareTo(a));
 }
