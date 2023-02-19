571a3a94e25bc833ba8c6c266474a26b6bb8a927
Luke Pighetti <lukepighetti@gmail.com>
2023-02-19 15:44:51 -0500

    works, and toleranced

--- bin/nsum.dart
+++ bin/nsum.dart
@@ -1,29 +1,51 @@
 void main(List<String> arguments) {
   final target = 28;
-  // final choices = [3, 4, 5, 6, 7, 8, 11, 14]..sort();
-  final choices = [3, 4]..sort();
-  final maxLength = (target / choices.first).floor();
+  final choices = [3, 4, 5, 6, 7, 8, 11, 14].ascending;
+  final tolerance = 0;
 
-  final expandedChoices =
-      List.generate(maxLength, (_) => choices).fold([], (a, b) => [...b, ...a]);
-  print(expandedChoices);
+  final results = <String>{};
 
-  final subsets = getAllSubsets(expandedChoices);
-  final matchingSubsets =
-      subsets.where((e) => e.isNotEmpty && e.reduce((a, b) => a + b) == target);
-  final sortedMatchingSubsets =
-      matchingSubsets.map((e) => (e..sort()).toString()).toSet();
+  void visit([List<int> curr = const []]) {
+    final sum = curr.sum;
+    for (final choice in choices) {
+      final nextSum = sum + choice;
 
-  print(sortedMatchingSubsets);
+      if (nextSum.withinToleranceOf(tolerance, target)) {
+        final result = [...curr, choice].ascending;
+        results.add('${result.sum}: ${result.join(',')}');
+      }
+
+      if (nextSum < target) {
+        visit([...curr, choice]);
+      }
+    }
+  }
+
+  final stopwatch = Stopwatch()..start();
+  visit();
+  stopwatch.stop();
+
+  results.forEach(print);
+
+  print(
+    'Found ${results.length} results '
+    'in ${stopwatch.elapsedMicroseconds / 1000}ms',
+  );
 }
 
-List<List<int>> getAllSubsets(List l) =>
-    l.fold<List<List<int>>>([[]], (subLists, element) {
-      return subLists
-          .map((subList) => [
-                subList,
-                subList + [element]
-              ])
-          .expand((element) => element)
-          .toList();
-    });
+extension ComparableList<T extends Comparable> on List<T> {
+  List<T> get ascending => List.from(descending.reversed);
+  List<T> get descending => List.from(this)..sort((a, b) => b.compareTo(a));
+}
+
+extension on List<int> {
+  int get sum => isEmpty ? 0 : reduce((a, b) => a + b);
+}
+
+extension on int {
+  bool withinToleranceOf(int tolerance, int target) {
+    final upperBound = target + tolerance;
+    final lowerBound = target - tolerance;
+    return upperBound >= this && this >= lowerBound;
+  }
+}
